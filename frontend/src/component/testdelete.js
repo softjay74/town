a4580a4344e2c10614243c799efb0f8e4020fe48
Je D Ha <jeha.webdev@gmail.com>
2018-09-03 16:29:05 -0700

    update will delete component

---
+++ frontend/src/component/testdelete.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class WillDelete extends component {
+    render(){
+
+        return(
+            <h1>Will Delete</h1>
+        )
+
+    }
+
+}export default WillDelete
